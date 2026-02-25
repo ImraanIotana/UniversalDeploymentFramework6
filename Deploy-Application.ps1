47b87c7adf16ed5cb0a319b869e63f6e8f381970
Imraan Iotana <subpagina_ionisch.0a@icloud.com>
2026-02-25 07:23:15 +0100

    comments

--- Deploy-Application.ps1
+++ Deploy-Application.ps1
@@ -1,6 +1,42 @@
 ####################################################################################################
 <#
 .SYNOPSIS
+    Deploys and removes applications using the Universal Deployment Framework.
+.DESCRIPTION
+    This script is part of the Universal Deployment Framework (UDF) and provides a standardized, modular approach for installing and uninstalling applications.
+    It supports all common deployment asset types, including MSI, EXE, fonts, drivers, certificates, and other application resources.
+    The script can be executed manually or integrated into enterprise deployment platforms such as Microsoft Intune or Microsoft Configuration Manager (SCCM).
+    INTUNE USAGE: Package this script together with the application source files into an .intunewin package. Use the command lines below.
+    SCCM USAGE: Import the application into SCCM and configure the following command lines:
+    - Install: powershell.exe -ExecutionPolicy Bypass -File .\Deploy-Application.ps1 -Install -Verbose
+    - Uninstall: powershell.exe -ExecutionPolicy Bypass -File .\Deploy-Application.ps1 -Uninstall -Verbose
+.EXAMPLE
+    Deploy-Application.ps1
+.EXAMPLE
+    Deploy-Application.ps1 -Install
+.EXAMPLE
+    Deploy-Application.ps1 -Uninstall
+.INPUTS
+    Requires at minimum:
+        - A valid Application/Asset ID
+        - One or more Deployment Objects
+    Source files (MSI, EXE, fonts, drivers, certificates, icons, etc.) may be placed in the root folder or any subfolder. The framework automatically discovers and
+    resolves the required files. If multiple files share the same name, prepend the parent folder name to ensure uniqueness.
+.OUTPUTS
+    No objects are returned to the pipeline. All operational output is written to the host and logged to the deployment logfile.
+.NOTES
+    Version         : Defined in the DeploymentScriptVersion variable
+    Author          : Imraan Iotana
+    Creation Date   : January 2023
+    Last Update     : January 2026
+.COPYRIGHT
+    Copyright (C) Iotana. All rights reserved.
+#>
+####################################################################################################
+
+####################################################################################################
+<#
+.SYNOPSIS
     This function install and uninstalls a collection of applications. These can be MSI, exe, font, driver, etc.
 .DESCRIPTION
     This script can be used to deploy applications manually, and also with Intune or SCCM:

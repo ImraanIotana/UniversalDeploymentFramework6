55d4bbee2d46a517c8585aa10ac5932060d002e1
Imraan Iotana <subpagina_ionisch.0a@icloud.com>
2026-02-27 05:36:58 +0100

    Write-FullError

---
+++ Engines/FileSystemEngine.psm1
@@ -0,0 +1,102 @@
+#
+# Module 'FileSystemEngine.psm1'
+# Last Update: February 2026
+#
+
+####################################################################################################
+<#
+.SYNOPSIS
+    Opens a folder or highlights an item in Explorer after basic validation.
+.DESCRIPTION
+    This function provides two main functionalities: opening a specified folder in Explorer or opening a folder and highlighting a specific item within it.
+    It performs validation to ensure that the provided paths are valid and exist before attempting to open them.
+    The function uses the 'Invoke-Item' cmdlet to open folders and 'Start-Process' with 'explorer.exe' to open folders with highlighted items.
+.EXAMPLE
+    Open-Folder -Path C:\Demo
+    Opens the specified folder in Explorer.
+.EXAMPLE
+    Open-Folder -SelectItem C:\Demo\NewFolder
+    Opens the folder and highlights the specified item in Explorer.
+.INPUTS
+    [System.String]
+.OUTPUTS
+    No objects are returned to the pipeline. All operational output is written to the host and logged to the deployment logfile.
+.NOTES
+    Version         : 6.0.0.0
+    Author          : Imraan Iotana
+    Creation Date   : December 2025
+    Last Update     : February 2026
+.COPYRIGHT
+    This script is part of the Universal Deployment Framework. Copyright (C) Iotana. All rights reserved.
+#>
+####################################################################################################
+
+function Open-Folder {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true,ParameterSetName='OpenFolder',HelpMessage='The path of the folder that will be opened.')]
+        [Alias('Folder')]
+        [AllowEmptyString()][System.String]$Path,
+
+        [Parameter(Mandatory=$true,ParameterSetName='HighlightItem',HelpMessage='The item that will be highlighted when the folder is opened.')]
+        [AllowEmptyString()][System.String]$SelectItem
+    )
+    
+    begin {
+        # Function
+        [System.String]$ParameterSetName    = [System.String]$PSCmdlet.ParameterSetName
+         
+        # Input
+        [System.String]$FolderToOpen        = $Path
+        [System.String]$ItemToHighlight     = $SelectItem
+
+        # Handlers
+        [System.String]$HighlightPrefix    = '/select,"{0}"'
+    }
+    
+    process {
+        # VALIDATION
+        switch ($ParameterSetName) {
+            'OpenFolder'    {
+                if (Test-String -IsEmpty $FolderToOpen) { Write-Line "The Path string is empty." -Type Fail ; Return }
+                if (-Not(Test-Path -Path $FolderToOpen)) { Write-Line "The Folder could not be reached. ($FolderToOpen)" -Type Fail ; Return }
+            }
+            'HighlightItem' {
+                if (Test-String -IsEmpty $ItemToHighlight) { Write-Line "The SelectItem string is empty." -Type Fail ; Return }
+                if (-Not(Test-Path -Path $ItemToHighlight)) { Write-Line "The selected item could not be reached. ($ItemToHighlight)" -Type Fail ; Return }
+            }
+        }
+
+        # EXECUTION
+        switch ($ParameterSetName) {
+            'OpenFolder'    {
+                # Open the folder
+                try {
+                    Write-Line "Opening folder... ($FolderToOpen)"
+                    Invoke-Item -Path $FolderToOpen
+                }
+                catch {
+                    Write-FullError
+                }
+            }
+            'HighlightItem' {
+                # Open the folder
+                try {
+                    Write-Line "Selecting item... ($ItemToHighlight)"
+                    Start-Process explorer.exe -ArgumentList ($HighlightPrefix -f $ItemToHighlight)
+                }
+                catch {
+                    Write-FullError
+                }
+            }
+        }
+
+    }
+    
+    end {
+    }
+}
+
+### END OF SCRIPT
+####################################################################################################
+
--- Engines/WriteEngine.psm1
+++ Engines/WriteEngine.psm1
@@ -16,7 +16,7 @@
 .INPUTS
     [System.String]
 .OUTPUTS
-    This function returns no stream output.
+    No objects are returned to the pipeline. All operational output is written to the host and logged to the deployment logfile.
 .NOTES
     Version         : 6.0.0.0
     Author          : Imraan Iotana
@@ -127,3 +127,144 @@ function Write-Line {
 
 ### END OF FUNCTION
 ####################################################################################################
+
+
+####################################################################################################
+<#
+.SYNOPSIS
+    Writes the full details of an error to the host and logs it to a file.
+.DESCRIPTION
+    Provides a function for writing the full details of an error to the host in yellow color, including the error message, exception type, invoking function hierarchy and error details.
+    The error is also logged to a file in the log folder.
+.EXAMPLE
+    Write-FullError -Message 'The file could not be found.'
+.EXAMPLE
+    Write-FullError
+.INPUTS
+    [System.String]
+    [System.Management.Automation.SwitchParameter]
+.OUTPUTS
+    No objects are returned to the pipeline. All operational output is written to the host and logged to the deployment logfile.
+.NOTES
+    Version         : 6.0.0.0
+    Author          : Imraan Iotana
+    Creation Date   : June 2023
+    Last Update     : February 2026
+.COPYRIGHT
+    This script is part of the Universal Deployment Framework. Copyright (C) Iotana. All rights reserved.
+#>
+####################################################################################################
+
+function Write-FullError {
+    [CmdletBinding(DefaultParameterSetName='UnknownError')]
+    param (
+        [Parameter(Mandatory=$true,ParameterSetName='Message',Position=0,HelpMessage='Your custom message that will be written in yellow.')]
+        [System.String]
+        $Message,
+
+        [Parameter(Mandatory=$false,ParameterSetName='UnknownError',Position=0,HelpMessage='Switch for using the default message for unknown errors.')]
+        [System.Management.Automation.SwitchParameter]
+        $UnknownError
+    )
+    
+    begin {
+        ####################################################################################################
+        ### MAIN OBJECT ###
+
+        # Set the main object
+        [PSCustomObject]$Local:MainObject = @{
+            # Function
+            ParameterSetName        = [System.String]$PSCmdlet.ParameterSetName
+            # Log Handlers
+            TimeStamp               = [System.String]((Get-Date -UFormat '%Y%m%d%R') -replace ':','')
+            LogFolder               = [System.String]($Global:DeploymentObject.LogFolder)
+            LogFileNameCircumFix    = [System.String]"$($Global:DeploymentObject.DeploymentData.ApplicationID)_Errorlog_{0}.log"
+            # Text Handlers
+            UnknownError            = [System.String]'An unknown error has occured.'
+            InvokingHierarchy       = [System.String]'InvocationOrder    : '
+            # Input
+            InputMessage            = $Message
+        }
+
+        ####################################################################################################
+        ### MAIN FUNCTION METHODS ###
+        
+        # Add the Begin method
+        Add-Member -InputObject $Local:MainObject -MemberType ScriptMethod -Name Begin -Value {
+            # Add the logfile path to the main object
+            [System.String]$LogFileName = ($this.LogFileNameCircumFix -f $this.TimeStamp)
+            Add-Member -InputObject $this -NotePropertyName LogFilePath -NotePropertyValue (Join-Path -Path $this.LogFolder -ChildPath $LogFileName)
+            # Add the InvokingFunctionHierarchy to the main object
+            $this.AddInvokingFunctionHierarchyToMainObject()
+        }
+
+        ####################################################################################################
+        ### MAIN PROCESSING METHODS ###
+
+        # Add the WriteFullError method
+        Add-Member -InputObject $Local:MainObject -MemberType ScriptMethod -Name WriteFullError -Value {
+            # Set the error message
+            [System.String]$ErrorMessage = switch ($this.ParameterSetName) {
+                'Message'       { $this.InputMessage }
+                'UnknownError'  { $this.UnknownError }
+            }
+            # Set the full name of the error
+            [System.String]$ErrorFullName = $Error[0].Exception.GetType().FullName
+            # In case of an unknown error, write all error details
+            if ($this.ParameterSetName -eq 'UnknownError') {
+                Start-Transcript -Path $this.LogFilePath -Append
+                Write-Host ('ERRORMESSAGE       : {0}' -f $ErrorMessage) -ForegroundColor Yellow
+                Write-Host ('ExceptionType      : ') -ForegroundColor Yellow -NoNewline
+                Write-Host $ErrorFullName
+                Write-Host $this.InvokingHierarchy -ForegroundColor Yellow -NoNewline
+                Write-Host $this.InvokingFunctionHierarchy
+                Write-Host ('Details            :') -ForegroundColor Yellow
+                $Error[0] | Out-Host
+                Write-Host
+                Stop-Transcript
+                # Open the logfolder
+                Open-Folder -SelectItem $this.LogFilePath
+            } else {
+                # Else only write the error message
+                Write-Host ('ERRORMESSAGE: {0}' -f $ErrorMessage) -ForegroundColor Yellow
+                Write-Host $this.InvokingHierarchy -ForegroundColor Yellow -NoNewline
+                Write-Host $this.InvokingFunctionHierarchy
+            }
+        }
+
+        ####################################################################################################
+        ### SUPPORTING METHODS ###
+
+        # Add the AddInvokingFunctionHierarchyToMainObject method
+        Add-Member -InputObject $Local:MainObject -MemberType ScriptMethod -Name AddInvokingFunctionHierarchyToMainObject -Value {
+            # Set the indeces of the invoking functions
+            [int[]]$IndecesOfInvokingFunctions = @(0..20)
+            # Get the array of the invoking functions
+            [string[]]$InvokingFunctionsArray = (Get-PSCallStack).Command
+            # Fill the InvokingFunctionHierarchy
+            [System.String]$InvokingFunctionHierarchy = [System.String]::Empty
+            $IndecesOfInvokingFunctions.ForEach({
+                # Get the name of the invoking function
+                [System.String]$InvokingFunctionName = $InvokingFunctionsArray[$_]
+                if ($InvokingFunctionName) { $InvokingFunctionHierarchy += ('[{0}] {1} ' -f $_, $InvokingFunctionName) }
+            })
+            # Add the InvokingFunctionHierarchy to the main object
+            Add-Member -InputObject $this -NotePropertyName InvokingFunctionHierarchy -NotePropertyValue $InvokingFunctionHierarchy
+        }
+
+        ####################################################################################################
+
+        $Local:MainObject.Begin()
+    }
+    
+    process {
+        $Local:MainObject.WriteFullError()
+    }
+    
+    end {
+    }
+}
+
+### END OF SCRIPT
+####################################################################################################
+
